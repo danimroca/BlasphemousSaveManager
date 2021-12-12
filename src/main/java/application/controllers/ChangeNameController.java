8f67faddaa55f979576b2e61be0e21d3eb618f6f
Dani <daniel.roca@acrolinx.com>
2021-12-12 20:22:57 +0100

    Fixed save name change

--- src/main/java/application/controllers/ChangeNameController.java
+++ src/main/java/application/controllers/ChangeNameController.java
@@ -4,7 +4,10 @@ import application.model.Profile;
 import application.model.Save;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class ChangeNameController extends BaseController {
@@ -18,16 +21,42 @@ public class ChangeNameController extends BaseController {
         closeCurrentStage();
     }
 
-    public void accept() {
+    public void accept() throws IOException {
         loadProperties();
         String saveName = saveNameTextField.getText();
-        for (Profile profile:profiles) {
-            for (Save s:profile.getSaves()) {
+        File oldSaveName = null;
+        File f = null;
+        File f2 = null;
+        for (Profile p:profiles) {
+            for (Save s:p.getSaves()) {
                 if (s.equals(save)) {
+                    oldSaveName = s.getSaveFile();
+                    f = s.getSaveFile();
+                    f2 = s.getBackupSaveFile();
                     s.setName(saveName);
                 }
             }
         }
+
+        File saveFolder = new File(f.getParentFile().getParent() + File.separator + saveName);
+        if (!saveFolder.exists()) {
+            saveFolder.mkdir();
+        }
+
+        FileUtils.moveFileToDirectory(f, saveFolder, false);
+        FileUtils.moveFileToDirectory(f2, saveFolder, false);
+        FileUtils.deleteDirectory(oldSaveName.getParentFile());
+
+
+        for (Profile p:profiles) {
+            for (Save s:p.getSaves()) {
+                if (s.equals(save)) {
+                    s.setSaveFile(new File(saveFolder + File.separator + f.getName()));
+                    s.setBackupSaveFile(new File(saveFolder + File.separator + f2.getName()));
+                }
+            }
+        }
+
         savesProfilesToXMLFile(profiles);
         closeCurrentStage();
     }
