b68fd8a099602cd52c253dedc2535f3987e237f8
Dani <daniel.roca@acrolinx.com>
2021-12-13 22:30:43 +0100

    Renamed Main class to Launcher Added maven-jar-plugin plugin to the project to compile the project to a jar

--- src/main/java/application/Launcher.java
+++ src/main/java/application/Launcher.java
@@ -7,7 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class Main extends Application {
+public class Launcher extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
